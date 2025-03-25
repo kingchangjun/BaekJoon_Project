d37566d348a5c14278b9d4d343340ecfe7edc62c
kingchangjun <nuj623@gmail.com>
2025-03-25 23:22:45 +0900

    lets go

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,46 +1,63 @@
 package org.example;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    /*
-    n > 123456 이므로 2n 은 최대 246912 이다.
-    0 부터 시작하므로 246912 + 1
-    */
-    public static boolean[] prime = new boolean[246913];
+    static boolean[] primeArray = new boolean[1000001];
 
-    public static void main(String[] args) throws IOException {
-
+    //입력&출력 부분
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        get_prime();	// 소수를 얻는 메소드 실행
+        findPrime();
 
-        while(true) {
-            int n = Integer.parseInt(br.readLine());
-            if(n == 0) break;	// n 이 0 일경우 종료
+        int T = Integer.parseInt(br.readLine()); //테스트의 개수
 
-            int count = 0;	// 소수 개수를 셀 변수
+        for(int i = 0; i < T; i++){
 
-            for(int i = n + 1; i <= 2 * n; i++) {
-                if(!prime[i]) count++;
+            int N  = Integer.parseInt(br.readLine()); //2보다 큰 짝수
+
+            int partitionCount = 0; // 파티션의 짝수
+
+            if(N % 2 == 0 && N != 0) { //짝수인 경우만
+                // 순서만 다르고 두 소수가 같은 경우는 같은 파티션임으로 N/2한다.
+                for (int j = 2; j <= N / 2; j++) {
+                    // 두 수의 합이 N일때 그 두수가 모두 소수일 때
+                    if (!primeArray[j]) { // 소수일 때
+                        if (!primeArray[N - j]) { // N- 소수 = 소수일때
+                            partitionCount++;
+                        }
+                    }
+                }
+                bw.write(partitionCount + "\n");
+            }else{
+                bw.write(0 + " \n");
             }
-            System.out.println(count);
         }
+
+        br.close();
+
+        bw.flush();
+        bw.close();
     }
 
-    // 소수를 얻는 메소드
-    public static void get_prime() {
-        // 0 과 1 은 소수가 아니므로 ture
-        prime[0] = prime[1] = true;
+    //소수 판별 배열 만다는 메소드
+    static void findPrime(){
+        primeArray[0] = primeArray[1] = true;
 
-        for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-            if(prime[i]) continue;
-            for(int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
+        for (int i = 2; i < primeArray.length; i++) { // 미리 골드바흐의 추측에 따른 소수값 구하기
+
+            if (primeArray[i] == false) {
+
+                for (int j = 2; i * j < primeArray.length; j++) {
+                    primeArray[i * j] = true;
+                }
             }
+
         }
     }
-
 }
