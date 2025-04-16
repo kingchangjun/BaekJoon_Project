7fd87d53f2882e8fd2a376280233e05d149bd309
kingchangjun <nuj623@gmail.com>
2025-04-16 23:56:12 +0900

    lets goㄴㅇ

--- src/main/java/org/example/Main17413.java
+++ src/main/java/org/example/Main17413.java
@@ -1,50 +1,41 @@
 package org.example;
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main17413 {
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = bf.readLine();
         Stack<Character> stack = new Stack<>();
 
-        String str = br.readLine();
-        int len = str.length();
-
-        boolean tag = false;
-        for(int i = 0 ; i<len; i++)
-        {
-            if(str.charAt(i) =='<')
-            {
-                tag = true;
-
-                while(!stack.isEmpty())
-                    sb.append(stack.pop());
-
-                sb.append(str.charAt(i));
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') { // 열린 괄호면 스택에 추가.
+                stack.push('(');
+                continue;
             }
-            else if(str.charAt(i)=='>')
-            {
-                tag = false;
-                sb.append(str.charAt(i));
-            }
-            else if(tag ==true)
-                sb.append(str.charAt(i));
-            else if(tag == false){
-                if(str.charAt(i)==' ')
-                {
-                    while(!stack.isEmpty())
-                        sb.append(stack.pop());
-                    sb.append(str.charAt(i));
+            if (input.charAt(i) == ')') { // 닫힌 괄호일 경우,
+                stack.pop(); // 일단 stack에서 pop을 실행.
+
+                if (input.charAt(i - 1) == '(') { // 그 전 괄호가 열린 괄호면 레이저를 의미.
+                    result += stack.size(); // 현재 stack의 사이즈만큼 더해 줌.
+                } else { // 그 전 괄호가 닫힌 괄호면 레이저가 아님.
+                    result++; // 단순히 1을 더해 줌.
                 }
-                else
-                    stack.push(str.charAt(i));
             }
         }
-        while(!stack.isEmpty())
-            sb.append(stack.pop());
-        System.out.println(sb);
+
+        bw.write(result + "\n");
+        bw.flush();
+        bf.close();
+        bw.close();
     }
+
 }
