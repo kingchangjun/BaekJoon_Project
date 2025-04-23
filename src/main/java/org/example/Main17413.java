2af29c766a3b22ac76010e6220b36370e09b6788
kingchangjun <nuj623@gmail.com>
2025-04-23 23:42:59 +0900

    lets goㄴㅇ

--- src/main/java/org/example/Main17413.java
+++ src/main/java/org/example/Main17413.java
@@ -1,41 +1,57 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.Stack;
 
-public class Main17413 {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String input = bf.readLine();
+public class Main17413{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         Stack<Character> stack = new Stack<>();
+        String s = br.readLine();
 
-        int result = 0;
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) == '(') { // 열린 괄호면 스택에 추가.
-                stack.push('(');
-                continue;
-            }
-            if (input.charAt(i) == ')') { // 닫힌 괄호일 경우,
-                stack.pop(); // 일단 stack에서 pop을 실행.
-
-                if (input.charAt(i - 1) == '(') { // 그 전 괄호가 열린 괄호면 레이저를 의미.
-                    result += stack.size(); // 현재 stack의 사이즈만큼 더해 줌.
-                } else { // 그 전 괄호가 닫힌 괄호면 레이저가 아님.
-                    result++; // 단순히 1을 더해 줌.
+        boolean isTag = false;
+        for(char c : s.toCharArray())
+        {
+            if(c =='<')
+            {
+                while(!stack.isEmpty()){
+                    sb.append(stack.pop());
                 }
+                isTag = true;
+                sb.append(c);
             }
-        }
+            else if(c=='>')
+            {
+                isTag = false;
+                sb.append(c);
+            }
+            else if(isTag)
+            {
+                sb.append(c);
+            }
+            else
+            {
+                if (c==' ')
+                {
+                    while(!stack.isEmpty())
+                    {
+                        sb.append(stack.pop());
+                    }
+                    sb.append(' ');
+                }
+                else stack.push(c);
+            }
 
-        bw.write(result + "\n");
-        bw.flush();
-        bf.close();
-        bw.close();
+        }
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+
+        System.out.println(sb);
+
     }
 
 }
