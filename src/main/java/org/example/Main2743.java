dc0a3ca25e8beaacdec18a5ce7c5872adf327513
kingchangjun <nuj623@gmail.com>
2025-05-01 21:22:03 +0900

    lets goㄴㅇ

--- src/main/java/org/example/Main2743.java
+++ src/main/java/org/example/Main2743.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 
-public class Main10820 {
+public class Main2743 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
