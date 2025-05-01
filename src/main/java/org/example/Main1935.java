f80b00214560c5300e05207d137ba0b6819981fd
kingchangjun <nuj623@gmail.com>
2025-05-01 19:02:38 +0900

    lets goㄴㅇ

--- src/main/java/org/example/Main1935.java
+++ src/main/java/org/example/Main1935.java
@@ -1,34 +1,52 @@
 package org.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.util.Stack;
 
 
-public class Main1935 {
-    public static void main(String[] args)throws IOException {
+public class Main1935{
+
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line;
+        Stack<Double>stack = new Stack<>();
 
-        while ((line = br.readLine()) != null) {
-            int lower = 0;
-            int upper = 0;
-            int digit = 0;
-            int space = 0;
+        int n = Integer.parseInt(br.readLine());;
 
-            for (char c : line.toCharArray()) {
-                if (Character.isLowerCase(c)) {
-                    lower++;
-                } else if (Character.isUpperCase(c)) {
-                    upper++;
-                } else if (Character.isDigit(c)) {
-                    digit++;
-                } else if (c == ' ') {
-                    space++;
-                }
+        Double[] arr = new Double[n];
+        String s = br.readLine();
+
+        for(int i =0; i<arr.length ; i++)
+            arr[i] = Double.parseDouble(br.readLine());
+
+        for(int i = 0 ; i< s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(c>='A' && c<='Z')
+            {
+                stack.push(arr[c-'A']);
+            }
+            else{
+                double a = stack.pop();
+                double b = stack.pop();
+                stack.push(ppp(b,a,c));
             }
 
-            System.out.println(lower + " " + upper + " " + digit + " " + space);
         }
+        System.out.printf("%.2f\n",stack.pop());
+    }
+    public static double ppp(double x, double y, char c)
+    {
+        switch(c)
+        {
+            case '+': return x+y;
+            case '-': return x-y;
+            case '*': return x*y;
+            case '/': return x/y;
+        }
+        return 0;
+
+
     }
 }
