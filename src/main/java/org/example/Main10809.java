8095f711780a369b728ce1a9156a7428fb5a7c3d
kingchangjun <nuj623@gmail.com>
2025-05-01 20:37:24 +0900

    lets goㄴㅇ

--- src/main/java/org/example/Main10809.java
+++ src/main/java/org/example/Main10809.java
@@ -3,23 +3,27 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 
-public class Main10808 {
+public class Main10809 {
     public static void main(String[] args)throws IOException{
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] counting = new int[26];
+        int[] counting = new int[26] ;
+
+        for(int i = 0 ; i < counting.length ; i++)
+            counting[i] = -1;
 
         String s = br.readLine();
 
-        for(int i = 0 ; i<s.length() ; i++)
+        for(int i =0 ;  i< s.length() ; i++)
         {
             char c = s.charAt(i);
-            counting[c-'a']++;
+            if(counting[c-'a'] == -1)
+                counting[c-'a'] = i;
         }
-        for(int num:counting)
+        for(int num : counting)
             System.out.print(num+" ");
+
 
     }
 }
