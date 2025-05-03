b87a031d9eb254ffc5da410bd1c574c0a3725fa6
kingchangjun <nuj623@gmail.com>
2025-05-03 21:16:59 +0900

    h2

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -2,25 +2,24 @@ package org.example;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+
 
 public class Main_today {
     public static void main(String[] args)throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        String line = br.readLine();
 
-        String A = st.nextToken();
-        String B = st.nextToken();
-        String C = st.nextToken();
-        String D = st.nextToken();
+        String[] suff = new String[line.length()];
 
-        String x = A+B;
-        String y = C+D;
+        for(int i =0 ; i< line.length() ; i++)
+        {
+            suff[i] = line.substring(i);
+        }
 
-        long first = Long.parseLong(x);
-        long second = Long.parseLong(y);
-
-        System.out.println(first+second);
-
-    }
+       Arrays.sort(suff);
+        for(String s : suff)
+            System.out.println(s);
+  }
 }
