5a083a8ea157f2667ec935f86aef997ef189def9
kingchangjun <nuj623@gmail.com>
2025-05-06 22:22:32 +0900

    h2

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -2,24 +2,32 @@ package org.example;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
-
+//백준 2609
 public class Main_today {
     public static void main(String[] args)throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        String[] suff = new String[line.length()];
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = gcd(a,b);
 
-        for(int i =0 ; i< line.length() ; i++)
-        {
-            suff[i] = line.substring(i);
-        }
+        System.out.println(c+"\n"+a*b/c);
 
-       Arrays.sort(suff);
-        for(String s : suff)
-            System.out.println(s);
+
   }
+  public static int gcd(int a , int b )
+  {
+      while(b!= 0)
+      {
+          int r = a%b;
+          a = b;
+          b = r;
+      }
+      return a;
+  }
+
 }
