e323fefddc68f6cb9a6ea1960a75ea823b147099
kingchangjun <nuj623@gmail.com>
2025-05-08 22:24:24 +0900

    h2

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -3,29 +3,51 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-//백준 2609
+
+//백준 6588
 public class Main_yesterday {
+    static final int Max = 1000000;
+    static boolean[] isnotPrime = new boolean[Max+1];
+
     public static void main(String[] args)throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+     BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+     ic();
 
-        int a = Integer.parseInt(st.nextToken());
-        int b= Integer.parseInt(st.nextToken());
-        System.out.println(aa(a,b));
-        System.out.println(bb(a,b));
+     while(true)
+     {
+         int n = Integer.parseInt(br.readLine());
+         if(n==0) break;
+         boolean hub = false;
+         for(int a = 2 ; a<=n/2; a++)
+         {
+             int b = n-a;
+             if(!isnotPrime[a] && !isnotPrime[b])
+             {
+                 System.out.println(n+" = "+a+" + "+b);
+                 hub = true;
+                 break;
+             }
+         }
+         if(!hub)
+             System.out.println("Goldbach's conjecture is wrong.");
+
+     }
 
     }
-
-    public static int aa(int a, int b)
+    public static void ic()
     {
-        if(b!=0) return aa(b,a%b);
-        return a;
-    }
-    public static int bb(int a, int b)
-    {
-        return a*b/aa(a,b);
+        isnotPrime[0] = isnotPrime[1] = true;
 
+        for(int i = 2; i*i<=Max ; i++)
+        {
+            if(!isnotPrime[i])
+            {
+                for(int j = i*i ; j<=Max ; j+=i)
+                {
+                    isnotPrime[j] = true;
+                }
+            }
+        }
     }
 }
