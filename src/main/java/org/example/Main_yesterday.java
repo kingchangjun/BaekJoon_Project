e084d5662675fdd735b6f7c59d57075ed0a60b48
kingchangjun <nuj623@gmail.com>
2025-07-13 17:32:08 +0900

    h2

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -1,53 +1,33 @@
 package org.example;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
+class Main_yesterday{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-//백준 6588
-public class Main_yesterday {
-    static final int Max = 1000000;
-    static boolean[] isnotPrime = new boolean[Max+1];
+        String a = br.readLine();
 
-    public static void main(String[] args)throws IOException {
-     BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-     ic();
-
-     while(true)
-     {
-         int n = Integer.parseInt(br.readLine());
-         if(n==0) break;
-         boolean hub = false;
-         for(int a = 2 ; a<=n/2; a++)
-         {
-             int b = n-a;
-             if(!isnotPrime[a] && !isnotPrime[b])
-             {
-                 System.out.println(n+" = "+a+" + "+b);
-                 hub = true;
-                 break;
-             }
-         }
-         if(!hub)
-             System.out.println("Goldbach's conjecture is wrong.");
-
-     }
-
-    }
-    public static void ic()
-    {
-        isnotPrime[0] = isnotPrime[1] = true;
-
-        for(int i = 2; i*i<=Max ; i++)
+        int count = (3-a.length() %3)%3;
+        for(int i = 0 ; i< count; i++)
         {
-            if(!isnotPrime[i])
-            {
-                for(int j = i*i ; j<=Max ; j+=i)
-                {
-                    isnotPrime[j] = true;
-                }
-            }
+            sb.append("0");
         }
+        sb.append(a);
+        String b = sb.toString();
+        sb.setLength(0);
+
+        for(int i =0 ; i<b.length() ; i+=3)
+        {
+            String c = b.substring(i,i+3);
+           sb.append(Integer.parseInt(c,2));
+
+        }
+        System.out.println(sb);
+
     }
+
 }
