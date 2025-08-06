d38f581bc92e479275dc96f1482056cdc9e8db5e
kingchangjun <nuj623@gmail.com>
2025-08-06 16:00:25 +0900

    h2

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -11,18 +11,14 @@ class Main_yesterday{
         int n = Integer.parseInt(br.readLine());
 
         int[] dp = new int[n+1];
-        dp[1] = 0;
-        for(int i = 2; i<=n ; i++){
-            dp[i] = dp[i-1]+1;
 
-            if(i%2==0)
-            {
-                dp[i] = Math.min(dp[i],dp[i/2]+1);
-            }
-            if(i%3==0)
-                dp[i] = Math.min(dp[i], dp[i/3]+1);
+        dp[1] = 1;
+        if(n>=2)dp[2] = 2;
+
+        for(int i =3 ; i<=n ; i++)
+        {
+            dp[i] = (dp[i-1]+dp[i-2])%10007;
         }
         System.out.println(dp[n]);
-
     }
 }
