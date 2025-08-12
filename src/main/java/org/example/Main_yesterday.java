e445822ee693e4c87f85b611f73a7f5bc6c1fc05
kingchangjun <nuj623@gmail.com>
2025-08-12 16:49:57 +0900

    h2

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -3,35 +3,34 @@ package org.example;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-
+//10844
 class Main_yesterday{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int mod = 1000000009;
-        int max = 100000;
+        int mod = 1000000000;
+        long[][] dp = new long [n+1][10];
 
-        long[][] dp = new long [max+1][4];
-        dp[1][1] = 1; dp[1][2] = 0; dp[1][3] = 0;
-        dp[2][1] = 0; dp[2][2] = 1; dp[2][3] = 0;
-        dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1;
+        for(int i = 1; i<=9 ; i++)
+            dp[1][i] = 1;
 
-        for(int i = 4; i<=max; i++)
+        for(int i = 2; i<=n; i++)
         {
-            dp[i][1] = (dp[i-1][2] + dp[i-1][3])%mod;
-            dp[i][2] = (dp[i-2][1] + dp[i-2][3])%mod;
-            dp[i][3] = (dp[i-3][2] + dp[i-3][1])%mod;
+            for(int j = 0; j<=9 ; j++)
+            {
+                if(j==0)
+                    dp[i][j] = dp[i-1][1] % mod;
+                else if(j==9)
+                    dp[i][j] = dp[i-1][8] % mod;
+                else
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%mod;
+            }
         }
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i =0; i<n; i++)
-        {
-            int x = Integer.parseInt(br.readLine());
-            long result = (dp[x][1] + dp[x][2] + dp[x][3])%mod;
-            sb.append(result).append("\n");
+        long result = 0;
+        for(int i = 0 ; i <=9; i++){
+            result = (result + dp[n][i]) % mod;
         }
-        System.out.println(sb);
+        System.out.println(result);
     }
 }
