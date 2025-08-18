83f97d4af27a09f2223d9a9ba8e58faa22f86d6b
kingchangjun <nuj623@gmail.com>
2025-08-18 18:47:33 +0900

    h2

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -1,40 +1,25 @@
 package org.example;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 class Main_today{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int mod = 1000000000;
 
         int n = Integer.parseInt(br.readLine());
-        long[][] dp = new long[n+1][10];
 
-        for(int i = 1 ; i<=9; i++)
-        {
-            dp[1][i] = 1;
-        }
+        long dp[] = new long[n+1];
 
-        for(int i = 2; i<=n ; i++)
+        dp[0] = 0; dp[1] = 1;
+
+        for(int i =2; i<=n; i++)
         {
-            for(int j = 0 ; j<=9 ; j++)
-            {
-                if(j==0)
-                    dp[i][j] = dp[i-1][1] % mod;
-                else if(j==9)
-                    dp[i][j] = dp[i-1][8] % mod;
-                else
-                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) & mod;
-            }
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        long result = 0;
-        for(int i =0; i<=9 ; i++)
-        {
-            result = (result + dp[n][i]) % mod;
-        }
-        System.out.println(result);
+        System.out.println(dp[n]);
+
 
     }
 }
--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -3,34 +3,22 @@ package org.example;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-//10844
+
 class Main_yesterday{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int mod = 1000000000;
-        long[][] dp = new long [n+1][10];
 
-        for(int i = 1; i<=9 ; i++)
-            dp[1][i] = 1;
+        int[] dp = new int[n+1];
 
-        for(int i = 2; i<=n; i++)
+        dp[0] = 0; dp[1] = 1;
+
+        for(int i= 2; i<=n;i++)
         {
-            for(int j = 0; j<=9 ; j++)
-            {
-                if(j==0)
-                    dp[i][j] = dp[i-1][1] % mod;
-                else if(j==9)
-                    dp[i][j] = dp[i-1][8] % mod;
-                else
-                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%mod;
-            }
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        long result = 0;
-        for(int i = 0 ; i <=9; i++){
-            result = (result + dp[n][i]) % mod;
-        }
-        System.out.println(result);
+
+        System.out.println(dp[n]);
     }
 }
