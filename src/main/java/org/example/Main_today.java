ab3cac590e2cb713f9d77e770a1d91859e54dc6a
kingchangjun <nuj623@gmail.com>
2025-08-19 17:11:03 +0900

    h2

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -1,24 +1,41 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main_today{
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
+        int[] arr = new int [n+1];
+        int[] dp = new int[n+1];
 
-        long dp[] = new long[n+1];
+        st = new StringTokenizer(br.readLine());
 
-        dp[0] = 0; dp[1] = 1;
-
-        for(int i =2; i<=n; i++)
+        for(int i = 1 ; i <= n; i++)
         {
-            dp[i] = dp[i-1]+dp[i-2];
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(dp[n]);
+
+        for(int i = 1; i<=n ; i++)
+        {
+            dp[i] = 1; // 최소 길이
+            for(int j = 1; j<i; j++)
+            {
+                if(arr[j] < arr[i] && dp[i] <dp[j] +1) // dp[j] -> arr[j]로 끝나는 최장 길이
+                    dp[i] = dp[j] + 1;
+            }
+        }
+
+        int max = -1;
+        for(int i =1; i<=n ; i++)
+            max = dp[i] > max ? dp[i] : max;
+        System.out.println(max);
+
+
 
 
     }
