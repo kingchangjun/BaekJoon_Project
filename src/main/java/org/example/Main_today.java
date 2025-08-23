d4bdc609b899b8185aa6e09fcd5ce054c456d790
kingchangjun <nuj623@gmail.com>
2025-08-23 17:13:23 +0900

    h2

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -1,41 +1,38 @@
 package org.example;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 class Main_today{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int [n+1];
+        int[] arr = new int[n+1];
         int[] dp = new int[n+1];
 
         st = new StringTokenizer(br.readLine());
-
-        for(int i = 1 ; i <= n; i++)
+        for(int i = 1; i<=n ; i++)
         {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 1; i<=n ; i++)
+        for(int i = 1 ; i<= n; i++)
         {
-            dp[i] = 1; // 최소 길이
+            dp[i] = 1;
             for(int j = 1; j<i; j++)
             {
-                if(arr[j] < arr[i] && dp[i] <dp[j] +1) // dp[j] -> arr[j]로 끝나는 최장 길이
-                    dp[i] = dp[j] + 1;
+                if(arr[i] > arr[j] && dp[j]+1 > dp[i] )
+                    dp[i] = dp[j] +1;
             }
         }
-
         int max = -1;
-        for(int i =1; i<=n ; i++)
+        for(int i = 1; i<=n; i++)
             max = dp[i] > max ? dp[i] : max;
         System.out.println(max);
-
-
 
 
     }
--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -1,24 +1,39 @@
 package org.example;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 class Main_yesterday{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-
+        int[] arr = new int[n+1];
         int[] dp = new int[n+1];
 
-        dp[0] = 0; dp[1] = 1;
-
-        for(int i= 2; i<=n;i++)
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i<=n ; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(dp[n]);
+        for(int i = 1 ; i<= n; i++)
+        {
+            dp[i] = 1;
+            for(int j = 1; j<i; j++)
+            {
+                if(arr[i] > arr[j] && dp[j]+1 > dp[i] )
+                    dp[i] = dp[j] +1;
+            }
+        }
+        int max = -1;
+        for(int i = 1; i<=n; i++)
+            max = dp[i] > max ? dp[i] : max;
+        System.out.println(max);
+
+
     }
 }
