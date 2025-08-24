41fb6e32d621d1c65dc46f0d5cc6384d9c1f5ed2
kingchangjun <nuj623@gmail.com>
2025-08-24 17:28:17 +0900

    pp

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -1,39 +1,65 @@
 package org.example;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.StringTokenizer;
+import java.util.Stack;
+import java.util.Arrays;
 
 class Main_yesterday{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
-        int[] dp = new int[n+1];
 
-        st = new StringTokenizer(br.readLine());
+        int[] dp = new int[n+1];
+        int[] arr = new int[n+1];
+        int[] prev = new int[n+1];
+
+        Arrays.fill(prev,-1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i<=n ; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int maxL = 0;
+        int lastIdx = -1;
+
         for(int i = 1; i<=n ; i++)
         {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for(int i = 1 ; i<= n; i++)
-        {
-            dp[i] = 1;
-            for(int j = 1; j<i; j++)
+            dp[i]=1;
+            for(int j = 1; j<=i ; j++)
             {
-                if(arr[i] > arr[j] && dp[j]+1 > dp[i] )
+                if(arr[j] < arr[i] && dp[i] < dp[j]+1)
+                {
                     dp[i] = dp[j] +1;
+                    prev[i] = j;
+                }
+            }
+
+            if(dp[i] > maxL)
+            {
+                maxL = dp[i];
+                lastIdx = i;
             }
         }
-        int max = -1;
-        for(int i = 1; i<=n; i++)
-            max = dp[i] > max ? dp[i] : max;
-        System.out.println(max);
+        StringBuilder sb = new StringBuilder();
+        sb.append(maxL+"\n");
+
+        Stack <Integer> stack = new Stack<>();
+        for(int c = lastIdx ; c != -1; c=prev[c])
+        {
+            stack.push(arr[c]);
+        }
+        while(!stack.isEmpty())
+            sb.append(stack.pop()+" ");
+
+        System.out.println(sb);
+
+
+
 
 
     }
+
 }
