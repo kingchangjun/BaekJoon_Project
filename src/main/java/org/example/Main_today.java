f60a9f50787ca6066ea29ccf8f889ac8034db348
kingchangjun <nuj623@gmail.com>
2025-08-28 16:24:53 +0900

    p

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -1,60 +1,37 @@
 package org.example;
 
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.Stack;
-import java.util.Arrays;
 
 class Main_today{
-    public static void main(String[] args ) throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
         int[] dp = new int[n+1];
+        int[] arr = new int[n+1];
 
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1 ; i<= n ; i++)
+        st = new StringTokenizer(br.readLine());
+        for(int i =1 ; i<= n ;i++)
         {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        dp[1] = arr[1];
 
-        int[] prev = new int[n+1];
-        Arrays.fill(prev,-1); // -1로 초기화.
+        int maxInt = arr[1];
 
-        int maxLen = 0, lastIdx = -1;
-
-        for(int i =  1; i<= n ; i++)
+        for(int i  = 2 ; i<=n; i++)
         {
-            dp[i] = 1;
-            for(int j = 1 ; j<i ; j++)
-            {
-                if(arr[j] < arr[i] && dp[j] +1 > dp[i])
-                {
-                    dp[i] = dp[j]+1;
-                    prev[i] = j;
-                }
-            }
-
-            if(dp[i] > maxLen)
-            {
-                maxLen = dp[i];
-                lastIdx = i;
-            }
+            dp[i] =  Math.max(dp[i-1] + arr[i] , arr[i]);
+            maxInt = Math.max(maxInt,dp[i]);
         }
-        Stack<Integer> stack = new Stack<>();
-        for(int cur = lastIdx ; cur != -1 ; cur = prev[cur])
-            stack.push(arr[cur]);
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(maxLen).append('\n');
+        System.out.println(maxInt);
 
-        while(!stack.isEmpty())
-            sb.append(stack.pop()).append(' ');
-        System.out.println(sb);
+
 
     }
 }
