3a095e775452405600a1509b6f75b42ea2283a16
kingchangjun <nuj623@gmail.com>
2025-08-30 15:10:19 +0900

    pp

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -4,62 +4,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
-import java.util.Stack;
-import java.util.Arrays;
 
 class Main_yesterday{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
+        int[] arr =new int[n+1];
         int[] dp = new int[n+1];
-        int[] arr = new int[n+1];
-        int[] prev = new int[n+1];
 
-        Arrays.fill(prev,-1);
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i<=n ; i++)
+        for(int i =1 ; i<=n ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-
-        int maxL = 0;
-        int lastIdx = -1;
-
-        for(int i = 1; i<=n ; i++)
-        {
-            dp[i]=1;
-            for(int j = 1; j<=i ; j++)
-            {
-                if(arr[j] < arr[i] && dp[i] < dp[j]+1)
-                {
-                    dp[i] = dp[j] +1;
-                    prev[i] = j;
-                }
-            }
-
-            if(dp[i] > maxL)
-            {
-                maxL = dp[i];
-                lastIdx = i;
-            }
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(maxL+"\n");
 
-        Stack <Integer> stack = new Stack<>();
-        for(int c = lastIdx ; c != -1; c=prev[c])
+        int maxInt = arr[1];
+        dp[1] = arr[1];
+
+        for(int i =2 ; i<=n ; i++)
         {
-            stack.push(arr[c]);
+            dp[i] = Math.max(dp[i-1] + arr[i] , arr[i]);
+            maxInt = Math.max(dp[i],maxInt);
         }
-        while(!stack.isEmpty())
-            sb.append(stack.pop()+" ");
-
-        System.out.println(sb);
-
-
-
+        System.out.println(maxInt);
 
 
     }
-
 }
