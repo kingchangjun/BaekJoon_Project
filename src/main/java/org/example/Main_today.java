cda02d23622847cbb215550326a30ff677fde69f
kingchangjun <nuj623@gmail.com>
2025-08-30 16:06:00 +0900

    pp

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -3,35 +3,22 @@ package org.example;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+//1699
 class Main_today{
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
-        int[] arr = new int[n+1];
+    int n = Integer.parseInt(br.readLine());
+    int[] dp = new int[n+1];
 
-        st = new StringTokenizer(br.readLine());
-        for(int i =1 ; i<= n ;i++)
+    for(int i =1 ; i<=n ; i++)
+    {
+        dp[i] = i;
+        for(int j = 1 ; j*j <=i ; j++)
         {
-            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Math.min(dp[i - j*j] + 1,dp[i]);
         }
-        dp[1] = arr[1];
-
-        int maxInt = arr[1];
-
-        for(int i  = 2 ; i<=n; i++)
-        {
-            dp[i] =  Math.max(dp[i-1] + arr[i] , arr[i]);
-            maxInt = Math.max(maxInt,dp[i]);
-        }
-
-        System.out.println(maxInt);
-
-
-
+    }
+    System.out.println(dp[n]);
     }
 }
