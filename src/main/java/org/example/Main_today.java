f65cee40648ab05799c3050475400e2824be69b9
kingchangjun <nuj623@gmail.com>
2025-08-31 18:03:19 +0900

    pp

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -1,24 +1,34 @@
 package org.example;
 
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
-//1699
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+
 class Main_today{
-    public static void main(String[] args) throws IOException{
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int k;
+    static int[][] dp;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        dp = new int[k+1][n+1]; //n을 k개의 수로 만드는 방법의 수
 
-    int n = Integer.parseInt(br.readLine());
-    int[] dp = new int[n+1];
-
-    for(int i =1 ; i<=n ; i++)
-    {
-        dp[i] = i;
-        for(int j = 1 ; j*j <=i ; j++)
+        for(int i = 0;i<=k ; i++)
         {
-            dp[i] = Math.min(dp[i - j*j] + 1,dp[i]);
+            dp[i][0] = 1; //0을 k개의 수로 만드는 방법의 수는 항상 1
         }
-    }
-    System.out.println(dp[n]);
+        for(int i = 1; i<=k; i++)
+        {
+
+            for(int j = 1; j<=n; j++)
+            {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] % 1000000000;
+            }
+        }
+        System.out.println(dp[k][n]);
+
     }
 }
