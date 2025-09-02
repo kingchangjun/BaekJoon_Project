ea94f8a491e54d27db93ade9b8032637661a1fac
kingchangjun <nuj623@gmail.com>
2025-09-02 14:02:41 +0900

    pp

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -1,24 +1,34 @@
 package org.example;
 
 import java.io.IOException;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
 
 class Main_yesterday{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        for(int i =1; i<=n ; i++)
+        long[][] dp = new long[k+1][n+1];
+
+        for(int i =0 ; i<=k ; i++)
         {
-            dp[i] = i;
-            for(int j=1; j*j <= i ; j++)
+            dp[i][0] = 1;
+        }
+
+        for(int K= 1; K<=k ; K++)
+        {
+            for(int N =1 ; N<=n ; N++)
             {
-                dp[i] = Math.min(dp[i],dp[i - j*j]+1);
+                dp[K][N] = dp[K-1][N] + dp[K][N-1];
             }
         }
-        System.out.println(dp[n]);
+
+        System.out.println(dp[k][n]);
     }
+
 }
