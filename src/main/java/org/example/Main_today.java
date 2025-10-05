e369a41eeef8129cfbc72e37f4f63dc8dab169dd
kingchangjun <nuj623@gmail.com>
2025-10-05 16:02:40 +0900

    pp

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -3,32 +3,30 @@ package org.example;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
 
 class Main_today{
-    static int n;
-    static int k;
-    static int[][] dp;
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        dp = new int[k+1][n+1]; //n을 k개의 수로 만드는 방법의 수
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0;i<=k ; i++)
-        {
-            dp[i][0] = 1; //0을 k개의 수로 만드는 방법의 수는 항상 1
-        }
-        for(int i = 1; i<=k; i++)
-        {
+        StringBuilder sb = new StringBuilder();
+        int max = 1000000;
+        long[] dp = new long[max+1];
 
-            for(int j = 1; j<=n; j++)
-            {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] % 1000000000;
-            }
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i = 4; i<=max ; i++)
+        {
+            dp[i] = (dp[i-1] + dp[i-2] + dp[i-3])%1000000009;
         }
-        System.out.println(dp[k][n]);
+
+        for(int i = 1; i<=n;i++)
+        {
+            int x = Integer.parseInt(br.readLine());
+            sb.append(dp[x]).append("\n");
+        }
+        System.out.println(sb);
 
     }
 }
--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -1,34 +1,31 @@
 package org.example;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 class Main_yesterday{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int MAX = 1000000;
+        int[] dp =  new int[MAX+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
 
-        long[][] dp = new long[k+1][n+1];
-
-        for(int i =0 ; i<=k ; i++)
+        for(int i =4 ; i<=MAX;i++)
         {
-            dp[i][0] = 1;
+            dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009;
         }
 
-        for(int K= 1; K<=k ; K++)
+        StringBuilder sb = new StringBuilder();
+        for(int i =1 ;i <=n; i++)
         {
-            for(int N =1 ; N<=n ; N++)
-            {
-                dp[K][N] = dp[K-1][N] + dp[K][N-1];
-            }
+            int x = Integer.parseInt(br.readLine());
+            sb.append(dp[x]).append("\n");
         }
-
-        System.out.println(dp[k][n]);
+        System.out.println(sb);
     }
-
 }
