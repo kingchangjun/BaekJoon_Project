60415829093381db29c0303ba4596b6b6924f1ab
kingchangjun <nuj623@gmail.com>
2025-10-09 16:22:41 +0900

    pp

--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
-class Main_today{
+class Main_yesterday{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
