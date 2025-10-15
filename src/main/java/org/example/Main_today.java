0e6b5411e0a64447fcd4870402c5367777a714e0
kingchangjun <nuj623@gmail.com>
2025-10-15 19:19:24 +0900

    ppp

--- src/main/java/org/example/Main_today.java
+++ src/main/java/org/example/Main_today.java
@@ -3,37 +3,32 @@ package org.example;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
 
 class Main_today{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        if (n<=0){
-            System.out.println(0);
-            return;
-        }
-        int[] dp= new int[3];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int MOD = 9901;
+        int[][] dp = new int[n+1][3];
 
-        dp[0] = Integer.parseInt(st.nextToken());
-        dp[1] = Integer.parseInt(st.nextToken());
-        dp[2] = Integer.parseInt(st.nextToken());
-        for(int i =0; i<n-1; i++)
+        if(n >=1)
         {
-            st = new StringTokenizer(br.readLine()," ");
-            int r = Integer.parseInt(st.nextToken());
-            int g = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            int[] cr = new int[3];
-            cr[0] = r + Math.min(dp[1],dp[2]);
-            cr[1] = g + Math.min(dp[0],dp[2]);
-            cr[2] = b + Math.min(dp[1],dp[0]);
-
-            dp=cr;
+            dp[1][0] = 1;
+            dp[1][1] = 1;
+            dp[1][2] = 1;
         }
-        int sum = Math.min(dp[0],Math.min(dp[1],dp[2]));
+
+        for(int i =2; i<=n; i++)
+        {
+            dp[i][0] = ((dp[i-1][0] + dp[i-1][1] ) % MOD + dp[i-1][2] ) % MOD;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2] ) % MOD;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+        }
+        int sum = 0;
+        if(n>=1)
+            sum = ((dp[n][0] + dp[n][1]) % MOD + dp[n][2]) % MOD;
+        else
+            sum = 1;
         System.out.println(sum);
 
     }
--- src/main/java/org/example/Main_yesterday.java
+++ src/main/java/org/example/Main_yesterday.java
@@ -3,38 +3,37 @@ package org.example;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
 
 class Main_yesterday{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        if (n<=0){
-            System.out.println(0);
-            return;
-        }
-        int[] dp= new int[3];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+    public static void main(String[] args) throws IOException{
+     BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
 
-        dp[0] = Integer.parseInt(st.nextToken());
-        dp[1] = Integer.parseInt(st.nextToken());
-        dp[2] = Integer.parseInt(st.nextToken());
-        for(int i =0; i<n-1; i++)
-        {
-            st = new StringTokenizer(br.readLine()," ");
-            int r = Integer.parseInt(st.nextToken());
-            int g = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+     int n = Integer.parseInt(br.readLine());
+     if(n<0)
+     {
+         System.out.println("1");
+         return;
+     }
+     int M = 9901;
 
-            int[] cr = new int[3];
-            cr[0] = r + Math.min(dp[1],dp[2]);
-            cr[1] = g + Math.min(dp[0],dp[2]);
-            cr[2] = b + Math.min(dp[1],dp[0]);
+     //사자가 없는 경우 1 왼 2 오 3
+     int [][] dp = new int[n+1][3+1];
 
-            dp=cr;
-        }
-        int sum = Math.min(dp[0],Math.min(dp[1],dp[2]));
-        System.out.println(sum);
+         dp[1][1] = 1;
+         dp[1][2] = 1;
+         dp[1][3] = 1;
 
+     for(int i = 2; i<= n; i++)
+     {
+         dp[i][1] = ((dp[i-1][1] + dp[i-1][2]) % M + dp[i-1][3]) % M;
+         dp[i][2] = (dp[i-1][1] + dp[i-1][3]) % M;
+         dp[i][3] = (dp[i-1][1] + dp[i-1][2]) % M;
+
+     }
+    int sum = 0;
+
+        sum = ((dp[n][1] + dp[n][2]) % M + dp[n][3]) % M;
+
+    System.out.println(sum);
     }
 }
